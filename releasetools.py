6300c7adcc7d83dcdfc7ecdfe2ec99a4d84e48fa
N.Pikota <nelson-pikota@hotmail.com>
2024-06-23 11:19:15 +0200

    update

--- releasetools.py
+++ releasetools.py
@@ -1,5 +1,6 @@
+#!/bin/env python3
 #
-# Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2021 The LineageOS Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,72 +15,24 @@
 # limitations under the License.
 
 import common
-import os
 import re
-
-# The joined list of user image partitions of source and target builds.
-# - Items should be added to the list if new dynamic partitions are added.
-# - Items should not be removed from the list even if dynamic partitions are
-#   deleted. When generating an incremental OTA package, this script needs to
-#   know that an image is present in source build but not in target build.
-USERIMAGE_PARTITIONS = [
-    "product",
-]
-
-def GetUserImages(input_tmp, input_zip):
-  return {partition: common.GetUserImage(partition, input_tmp, input_zip)
-          for partition in USERIMAGE_PARTITIONS
-          if os.path.exists(os.path.join(input_tmp,
-                                         "IMAGES", partition + ".img"))}
-
-def FullOTA_GetBlockDifferences(info):
-  images = GetUserImages(info.input_tmp, info.input_zip)
-  return [common.BlockDifference(partition, image)
-          for partition, image in images.items()]
-
-def IncrementalOTA_GetBlockDifferences(info):
-  source_images = GetUserImages(info.source_tmp, info.source_zip)
-  target_images = GetUserImages(info.target_tmp, info.target_zip)
-
-  # Use EmptyImage() as a placeholder for partitions that will be deleted.
-  for partition in source_images:
-    target_images.setdefault(partition, common.EmptyImage())
-
-  # Use source_images.get() because new partitions are not in source_images.
-  return [common.BlockDifference(partition, target_image, source_images.get(partition))
-          for partition, target_image in target_images.items()]
 
 def FullOTA_InstallEnd(info):
   OTA_InstallEnd(info)
+  return
 
 def IncrementalOTA_InstallEnd(info):
   OTA_InstallEnd(info)
-
-def FullOTA_Assertions(info):
-  AddTrustZoneAssertion(info, info.input_zip)
-
-def IncrementalOTA_Assertions(info):
-  AddTrustZoneAssertion(info, info.target_zip)
+  return
 
 def AddImage(info, basename, dest):
-  path = "IMAGES/" + basename
-  if path not in info.input_zip.namelist():
-    return
-
-  data = info.input_zip.read(path)
-  common.ZipWriteStr(info.output_zip, basename, data)
+  name = basename
+  data = info.input_zip.read("IMAGES/" + basename)
+  common.ZipWriteStr(info.output_zip, name, data)
   info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
-  info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
-
-def AddTrustZoneAssertion(info, input_zip):
-  android_info = info.input_zip.read("OTA/android-info.txt")
-  m = re.search(r'require\s+version-trustzone\s*=\s*(\S+)', android_info)
-  if m:
-    versions = m.group(1).split('|')
-    if len(versions) and '*' not in versions:
-      cmd = 'assert(samsung.verify_trustzone(' + ','.join(['"%s"' % tz for tz in versions]) + ') == "1" || abort("ERROR: This package requires firmware from an Android 11 based stock ROM build. Please upgrade firmware and retry!"););'
-      info.script.AppendExtra(cmd)
+  info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
 def OTA_InstallEnd(info):
-  AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
-  AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
+  AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo")
+  AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta")
+  return
